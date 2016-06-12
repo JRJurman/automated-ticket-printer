180ab366bf77ccde6bb40371572745c9c47b83b6
Jesse Jurman <j.r.jurman@gmail.com>
2016-06-12 05:40:03 -0400

    example script that puts it all together

---
+++ example_scripts/console-watch.js
@@ -0,0 +1,7 @@
+var ActivityWatcher = require('../dist/ticket-printer').ActivityWatcher;
+var consolePrinter = require('../dist/ticket-printer').consolePrinter;
+var timeWatch = require('../dist/ticket-printer').timeWatch;
+
+var aw = new ActivityWatcher({printLogs:true});
+aw.addPrinter(consolePrinter);
+aw.addWatch(timeWatch, 5000);
